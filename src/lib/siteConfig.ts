4d55d4f9df242bea07676e356c9054d48602fbdf
Benjamin Shuster <77933963+bentzi-shuster@users.noreply.github.com>
2024-11-12 13:27:18 -0500

    cleanup

--- src/lib/siteConfig.ts
+++ src/lib/siteConfig.ts
@@ -1,6 +1,24 @@
+import roles from "./config/roles.json";
+import committees from "./config/committees.json";
 interface SiteConfig {
   clubDataUrl: string;
   announcmentsSheetUrl: string;
+  roles: {
+    [key: string]: {
+      label: string;
+      content: {
+        title: string;
+        description: string;
+      };
+    }[];
+  };
+  committees: {
+    [key: string]: {
+      label: string;
+      abbreviation: string;
+      description: string;
+    };
+  };
 }
 
 export let siteConfig: SiteConfig = {
@@ -8,4 +26,6 @@ export let siteConfig: SiteConfig = {
     "https://njit.campuslabs.com/engage/api/discovery/search/organizations?orderBy[0]=UpperName asc&top=150&filter=(BranchId%20eq%20296346)",
   announcmentsSheetUrl:
     "https://docs.google.com/spreadsheets/d/11pXrj7FqBO5hMXyJgBB8XwrXfiBXs-nMxPtudWqcjnw/export?format=csv",
+  roles: roles,
+  committees: committees,
 };
