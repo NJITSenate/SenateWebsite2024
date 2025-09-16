f3a1bf0a45ee013dcbd0e45aa101eb3d4e216978
Timothy Bailey <tsb32@njit.edu>
2025-09-16 19:04:13 -0400

    Nandini requests (Public Constitution (25-26 and 25-26 Senators page correction)

--- src/lib/siteConfig.ts
+++ src/lib/siteConfig.ts
@@ -44,7 +44,7 @@ export let siteConfig: SiteConfig = {
   clubDataUrl:
     "https://njit.campuslabs.com/engage/api/discovery/search/organizations?orderBy[0]=UpperName asc&top=150&filter=(BranchId%20eq%20296346)",
   archive: {
-    "2425": {
+    "2024 - 2025": {
       pagesFolderID: "1oTTXkMehSivsM1MM4vmqL3u6XRgIpLai",
       announcmentsSheetUrl:
         "https://docs.google.com/spreadsheets/d/11pXrj7FqBO5hMXyJgBB8XwrXfiBXs-nMxPtudWqcjnw/export?format=csv",
